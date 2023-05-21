4d8de6a7299e88a6d4ac8ecc3eaacb038fb51fbf
Alejandro Reyes <arcaela.reyes@gmail.com>
2023-05-21 00:15:59 -0400

    Add Test suites for more methods

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,41 +1,65 @@
-import type { IObject } from "@arcaelas/utils"
-import { Collection } from "../src"
 import items from './items.json'
+import { Collection } from "../src"
 
-let collection: Collection<typeof items extends [infer T] ? T : IObject>
+let collection: Collection<typeof items[0]>
 
 beforeAll(() => {
-    collection = new Collection<typeof items extends [infer T, ...any] ? T : IObject>(items as [])
+    collection = new Collection(items)
 })
 
-test("Collection created!", () => {
-    expect(collection).toBeInstanceOf(Collection)
+test('macro', () => {
+    collection.macro('test', () => 12)
+    expect(collection.test()).toEqual(12)
 })
-
-test("join", () => {
-    expect(collection.join("name", ",", "and")).toMatch(/\w+,/g)
-    expect(collection.join("name", ",", "and")).toMatch(/ and \w+/g)
+test('macroStatic', () => {
+    Collection.macro('testStatic', () => 12)
+    expect(collection.testStatic()).toEqual(12)
 })
-
-test("map", () => {
-    expect(collection.map(item => item.name).join(",")).toEqual(items.map(item => item.name).join(","))
+test('collect', () => {
+    expect(collection.collect([]).test()).toEqual(12)
 })
-
-test("pop", () => {
-    expect(collection.pop()).toEqual(items[items.length - 1])
+test('chunk', () => {
+    const chunk = collection.chunk(4)
+    expect(chunk.length).toEqual(Math.ceil(items.length / 4))
+    expect(chunk[0][0]).toMatchObject(items[0])
+    expect(chunk[chunk.length - 1].pop()).toMatchObject(items[items.length - 1])
 })
-
+test('countBy', () => {
+    expect(collection.countBy('gender').female)
+        .toEqual(4)
+})
+test('delete', () => {
+    const e = collection.first({ index: 0 }) as any
+    collection.delete({ index: 0 })
+    expect(collection.at(0)?.index).toEqual(1)
+    collection.unshift(e)
+    expect(collection.at(0)?.index).toEqual(0)
+})
+test('dump', () => {
+    expect(collection.dump()).toEqual(collection)
+})
+test('each', () => {
+    let i = 0
+    collection.each(item => (i += item.index) < 7)
+    expect(i).toEqual(10)
+})
+test('every', () => {
+    expect(collection.every('_id')).toEqual(true)
+    expect(collection.every('index', 0)).toEqual(false)
+    expect(collection.every('index', '>=', 0)).toEqual(true)
+    expect(collection.every({ age: { $gt: 0 } })).toEqual(true)
+})
+test('filter', () => {
+    expect(collection.filter(i => i.index > 0).length).toEqual(6)
+    expect(collection.filter({ index: { $gte: 1 } }).length).toEqual(6)
+})
 test("first", () => {
     expect(collection.first()).toMatchObject(items[0])
-})
-
-test("firstQuery", () => {
     expect(collection.first({ email: "greerfisher@zentime.com" })).toMatchObject(items[1])
     expect(collection.first({ email: { $eq: "greerfisher@zentime.com" } })).toMatchObject(items[1])
     expect(collection.first({ tags: { $includes: "proident" } })).toMatchObject(items[1])
 })
-
-test("notQuery", () => {
-    expect(collection.first({ email: { $not: "greerfisher@zentime.com" } })).toMatchObject(items[0])
-    expect(collection.first({ email: { $not: "greerfisher@zentime.com" } })).toMatchObject(items[0])
+test("map", () => {
+    expect(collection.map(item => item.name).join(','))
+        .toEqual(items.map(item => item.name).join(","))
 })
