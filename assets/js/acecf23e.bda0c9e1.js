e43f7208acca31e92edc6813468ba9cbc9b0083b
Miguel Guevara <arcaela.reyes@gmail.com>
2025-06-26 05:38:26 -0400

    docs: actualizar documentación 2025-06-26 05:38:26

---
+++ assets/js/acecf23e.bda0c9e1.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[1903],{1912:o=>{o.exports=JSON.parse('{"blogBasePath":"/collection/blog","blogTitle":"Blog","authorsListPath":"/collection/blog/authors"}')}}]);
